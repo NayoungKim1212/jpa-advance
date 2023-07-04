ef783e88e9176406988fdcf03d54bc7a3526abf1
NayoungKim1212 <kkd04250@naver.com>
2023-07-04 16:51:51 +0900

    중간 테이블 Order 주문일 추가

--- src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -3,22 +3,30 @@ package com.sparta.jpaadvance.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    // 관계를 맺기 위해 직접 만들었기 때문에 각각 생성해줘야함
-    @ManyToOne // Many : 처음에 오는게 현재 Entity 클래스
-              // One : 두 번째 오는게 상대 Entity
+
+    @ManyToOne
     @JoinColumn(name = "food_id")
     private Food food;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime orderDate;
 }
