ffd0c916d1d0075f6baeb41d4471cf71c4006729
NayoungKim1212 <kkd04250@naver.com>
2023-07-05 15:30:24 +0900

    지연 로딩과 즉시 로딩

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
@@ -18,7 +15,12 @@ public class Food {
     private String name;
     private double price;
 
-    @OneToMany(mappedBy = "food") // N : 주문은 여러 번 될 수 있기 때문에
-    private List<Order> orderList = new ArrayList<>();
-    // Order 테이블을 통해서 User를 줘야 할 생각이 없다면 생략 가능
+    @ManyToOne //(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; // User와 관계가 맺어짐
+
+//    @OneToMany(mappedBy = "food") // N : 주문은 여러 번 될 수 있기 때문에
+//    private List<Order> orderList = new ArrayList<>();
+//    // Order 테이블을 통해서 User를 줘야 할 생각이 없다면 생략 가능
 }
+
--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -12,12 +12,23 @@ import java.util.List;
 @Setter
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user") // 고객은 One, 주문을 여러 번 할 수 있음
-    private List<Order> orderList = new ArrayList<>();
-    // Order 테이블을 통해서 Food를 줘야 할 생각이 없다면 생략 가능
+    @OneToMany(mappedBy = "user") // 외래 키 주인의 필드 : user
+    private List<Food> foodList = new ArrayList<>(); // 데이터베이스 테이블에 영향을 미치지 X
+
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//
+//    @OneToMany(mappedBy = "user") // 고객은 One, 주문을 여러 번 할 수 있음
+//    private List<Order> orderList = new ArrayList<>();
+//// Order 테이블을 통해서 Food를 줘야 할 생각이 없다면 생략 가능
 }
+
